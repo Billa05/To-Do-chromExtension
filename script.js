8ff9f89a0ebf9015f7a115671d25d2a3b1761af4
Biresh <workinguse5@gmail.com>
2024-02-10 21:23:27 +0530

    working logic

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+let json_data = [];
+load_data();
+
 document.getElementById('add-button').addEventListener('click', function() {
     var value = document.getElementById('todo-input').value;
     if (value) {
-        addItem(value);
+        save(value);
         document.getElementById('todo-input').value = '';
     }
 });
@@ -16,8 +19,36 @@ function addItem(text) {
     remove.innerText = 'Remove';
     remove.addEventListener('click', function() {
         list.removeChild(item);
+        const index = json_data.indexOf(text);
+        if (index !== -1) {
+            json_data.splice(index, 1);
+        }
+        update_data();
     });
 
     item.appendChild(remove);
     list.appendChild(item);
+}
+
+
+function save(data){
+    json_data.push(data);
+    update_data();
+    addItem(data);
+}
+
+function load_data(){
+    const loaded_data = localStorage.getItem("array");
+    const final_data = JSON.parse(loaded_data);
+    if (final_data){
+        json_data = final_data;
+        for(let i=0;i<final_data.length;i++){
+            addItem(final_data[i]);
+        }
+    }
+}
+
+function update_data(){
+    const jsonArray= JSON.stringify(json_data);
+    localStorage.setItem("array",jsonArray);
 }
